7ce21b18c2ab3919bbea5964d0c499f4b4089550
Hans Zhou <hans.zhou@cedar.com>
2020-12-04 09:31:05 -0500

    All work and no play makesjackadullboy

--- 2020/day4.js
+++ 2020/day4.js
@@ -27,3 +27,28 @@ part1 = (input) => {
 	return count
 }
 console.log(part1(input))
+
+part2 = (input) => {
+	fields = {
+		"byr": (x => x.match(/^[0-9]{4}$/) && x < 2003 && x > 1919),
+		"iyr": (x => x.match(/^[0-9]{4}$/) && x < 2021 && x > 2009),
+		"eyr": (x => x.match(/^[0-9]{4}$/) && x < 2031 && x > 2019),
+		"hgt": (x => (x.match(/^[0-9]{3}cm$/) && x < "194cm" && x> "149cm") || (x.match(/^[0-9]{2}in$/) && x < "77cm" && x> "58cm")),
+		"hcl": (x => x.match(/^#[0-9a-f]{6}$/)),
+		"ecl": (x => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(x)),
+		"pid": (x => x.match(/^[0-9]{9}$/)),
+	}
+	count = 0
+	for (var row of input) {
+		valid = true
+		for (var field in fields) {
+			if (row[field] === undefined || !fields[field](row[field])) {
+				valid = false
+				break
+			}
+		}
+		count += valid
+	}
+	return count
+}
+console.log(part2(input))
